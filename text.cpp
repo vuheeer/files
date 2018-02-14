1571b28f6859453254959b183b6b188b8cc6e3d8
vuheeer <vuheeer@users.noreply.github.com>
2018-02-14 10:39:15 -0400

    Added implemetation file

---
+++ text.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+	ofstream out{"abc.txt"};
+	if (out) {
+		out << a << endl;
+		out << b << endl;
+		out << c << endl;
+	}
+	else {
+		cout << "Could not write";
+	}
+	cout<<endl<<endl;
+ 
+   return 0;
+}
